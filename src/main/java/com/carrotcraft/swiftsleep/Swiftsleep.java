5f9cf9eeffcf5e26720284ca660ff0795c7001f8
BuildTools <unconfigured@null.spigotmc.org>
2020-06-22 15:55:43 -0400

    Setup initial project stuff

---
+++ src/main/java/com/carrotcraft/swiftsleep/Swiftsleep.java
@@ -0,0 +1,17 @@
+package com.carrotcraft.swiftsleep;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Swiftsleep extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
